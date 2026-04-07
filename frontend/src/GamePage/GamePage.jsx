70413769a230da5cb898227b4d5998cfbc88044f
web-coder <yasher18@bk.ru>
2026-04-08 00:40:11 +0300

    добавил кнопку и todo

--- frontend/src/GamePage/GamePage.jsx
+++ frontend/src/GamePage/GamePage.jsx
@@ -72,6 +72,11 @@ function GamePage() {
                     <strong>Критиков оценило { gameObject.countRating } раз</strong>
                 </div>
                 <div className='mt-5'>
+                    <h3>Добавить игру в корзину</h3>
+                    <button className='btn btn-outline-warning'>Добавить</button>
+                    {/* TOOD: потом добавить роут с получением и доабвлением в маисв юзера его игр */}
+                </div>
+                <div className='mt-5'>
                     <h4>Оценить игру metaCritic</h4>
 
                     {
@@ -80,6 +85,7 @@ function GamePage() {
                                 <button key = { index } className='btn btn-danger ml-3 mb-3' onClick={() => { setRaitingStar(starValue) }}>
                                     <i className="fas fa-star text-warning ml-auto"></i>
                                     <span>{ starValue }</span>
+                                    {/* TODO: в роуте при изменение эттго сделать чтоыб ыбал вызвана функция дял занесеня в истори user countStars gameId */}
                                 </button>
                             )
                         })
